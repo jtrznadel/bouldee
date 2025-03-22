6457ad120955df4ee0090db5f6bedae3f8bb69c2
jtrznadel <trznadeljakub@outlook.com>
2025-03-22 22:10:10 +0100

    init

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/bouldee/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.bouldee
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:bouldee/app/app.dart';
+import 'package:bouldee/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
